bfbc36730e8b1632f345e15f1cc2128e7a64abb3
RobinTheRedLight <robinmitra789@gmail.com>
2023-01-15 15:23:50 +0600

    Admin Delete Route Added

--- index.js
+++ index.js
@@ -141,6 +141,12 @@ async function run() {
             const result2 = await adDataCollection.deleteOne(query2);
             res.send(result1);
         });
+        app.delete('/users/delete/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await userCollection.deleteOne(query);
+            res.send(result);
+        });
     }
     finally {
 
